673698a5cf49056bb111a60fceff94a2eca2be2f
Thanh-Thong-03 <mathanhthong462@gmail.com>
2023-11-04 10:06:54 +0700

    Tao trang them lien he

--- src/router/index.js
+++ src/router/index.js
@@ -8,12 +8,18 @@ const routes = [
     path: "/:pathMatch(.*)*",
     name: "notfound",
     component: () => import("@/view/NotFound.vue"),
+  },
+  {
+    path:"/contact/create",
+    name:"contact.add",
+    component: () => import("@/view/CreateContact.vue"),
   },{
     path: "/contacts/:id",
     name: "contact.edit",
     component: () => import("@/view/ContactEdit.vue"),
     props: true // Truyền các biến trong $route.params vào làm props
     },
+    
 ];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/view/CreateContact.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page">
+    <h4>Thêm liên hệ</h4>
+    <ContactForm :contact="{}" @submit:contact="createContact" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+      <script>
+  import ContactForm from "@/components/ContactForm.vue";
+  import ContactService from "@/services/contact.service";
+  export default {
+    components: {
+      ContactForm,
+    },
+    data() {
+      return {
+        contact: null,
+        message: "",
+      };
+    },
+    methods: {
+    async createContact(data){
+        try {
+            await ContactService.create(data);
+            this.message = "Tao lien he thanh cong"
+        } catch (error) {
+            console.log(error);
+        }
+     }
+    }
+  };
+  </script>
